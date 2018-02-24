50ebe3821dfa6c8ed08b4ee40cc1d0cdd452b235
Pronyakin Sergey <spkobra2@gmail.com>
2018-02-24 22:10:55 +0300

    Финальная версия решения задачи #13

--- task13_add_to_cart.py
+++ task13_add_to_cart.py
@@ -1,52 +1,47 @@
-"""
-1) открыть главную страницу
-2) открыть первый товар из списка
-2) добавить его в корзину (при этом может случайно добавиться товар, который там уже есть, ничего страшного)
-3) подождать, пока счётчик товаров в корзине обновится
-4) вернуться на главную страницу, повторить предыдущие шаги ещё два раза,
-чтобы в общей сложности в корзине было 3 единицы товара
-5) открыть корзину (в правом верхнем углу кликнуть по ссылке Checkout)
-6) удалить все товары из корзины один за другим, после каждого удаления подождать, пока внизу обновится таблица
-"""
 from selenium import webdriver
-
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 wait = WebDriverWait(driver, 5)
 
+QUANTITY_XPATH = '//div[@id="cart"]//a[2]/span[@class="quantity"]'
+FIRST_ELEMENT = '//*[@id="box-most-popular"]//a[1]/div[2]'
+
 def add_to_cart():
+    """Функция добавления товара в корзину с последующим удалением"""
+
     driver.get('http://localhost/litecart/en/')
-    count_product = driver.find_element_by_xpath('//div[@id="cart"]//a[2]/span[@class="quantity"]')
-
-    while int(count_product.get_attribute('textContent')) < 10:
-        count_product = driver.find_element_by_xpath('//div[@id="cart"]//a[2]/span[@class="quantity"]')
-        first_product = driver.find_element_by_xpath('//*[@id="box-most-popular"]//a[1]/div[2]').click()
-        x = int(driver.find_element_by_xpath('//div[@id="cart"]//a[2]/span[@class="quantity"]').get_attribute(
-            'textContent')) + 1
-
-        if len(driver.find_elements_by_name('options[Size]')) > 0:
+    count_product = driver.find_element_by_xpath(QUANTITY_XPATH)
+    #Пока в корзине меньше N товаров, добавлять товары
+    while int(count_product.get_attribute('textContent')) < 3:
+        driver.find_element_by_xpath(QUANTITY_XPATH)
+        driver.find_element_by_xpath(FIRST_ELEMENT).click() #кликаем первый
+        # Присваиваем Х значение на 1 больше, чем ранее
+        x = int(driver.find_element_by_xpath(QUANTITY_XPATH).get_attribute('textContent')) + 1
+        if len(driver.find_elements_by_name('options[Size]')) > 0: #если на странице есть этот блок, то работаем с ним
             driver.find_element_by_name('options[Size]').click()
             driver.find_element_by_css_selector('select[name="options[Size]"] option[value="Small"]').click()
             driver.find_element_by_name('add_cart_product').click()
-            if int(driver.find_element_by_xpath('//div[@id="cart"]//a[2]/span[@class="quantity"]').get_attribute(
+            #Пока счетчик не станет равным X (то есть на 1 больше, чем до клика для добавления товара),ожидаем появления
+            #счетчика равным X (ожидаение появления нужного текста элемента)
+            if int(driver.find_element_by_xpath(QUANTITY_XPATH).get_attribute(
                 'textContent')) < x:
-                q = driver.find_element_by_xpath('//div[@id="cart"]//a[2]/span[@class="quantity"]')
-                wait.until(EC.text_to_be_present_in_element((By.XPATH, '//div[@id="cart"]//a[2]/span[@class="quantity"]'), str(x)))
-            driver.get('http://localhost/litecart/en/')
+                wait.until(EC.text_to_be_present_in_element((By.XPATH, QUANTITY_XPATH), str(x)))
+            driver.get('http://localhost/litecart/en/') #возвращаемся на главную после добавления товара
         else:
             driver.find_element_by_name('add_cart_product').click()
-            if int(driver.find_element_by_xpath('//div[@id="cart"]//a[2]/span[@class="quantity"]').get_attribute(
+            if int(driver.find_element_by_xpath(QUANTITY_XPATH).get_attribute(
                     'textContent')) < x:
-                wait.until(EC.text_to_be_present_in_element((By.XPATH, '//div[@id="cart"]//a[2]/span[@class="quantity"]'), str(x)))
+                wait.until(EC.text_to_be_present_in_element((By.XPATH, QUANTITY_XPATH), str(x)))
             driver.get('http://localhost/litecart/en/')
-        count_product = driver.find_element_by_xpath('//div[@id="cart"]//a[2]/span[@class="quantity"]')
-    driver.find_element_by_xpath('//div[@id="cart"]//a[3]').click() #переход в корзину
+        count_product = driver.find_element_by_xpath(QUANTITY_XPATH) #поиск элемента после каждого цикла
 
+    driver.find_element_by_xpath('//div[@id="cart"]//a[3]').click() #переход в корзину по завершению цикла
 
+    #Удаление элементов с ожиданием обновления таблицы
     remove_button_list = driver.find_elements_by_css_selector('li[class="item"] button[name="remove_cart_item"]')
     for button in remove_button_list:
         button_remove = driver.find_element_by_css_selector('div[class="viewport"] button[name="remove_cart_item"]')

393dd5e81b26da54f12d747c54d27f979c2ae401
unknown <spkobra2@gmail.com>
2018-03-09 13:46:27 +0300

    Задание 17

--- task17_log.py
+++ task17_log.py
@@ -1,12 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import time
 driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 5)
 
-#config
 LOGIN = 'admin'
 PASSWORD = 'admin'
 ADMIN_URL = 'http://localhost/litecart/admin/'
@@ -19,52 +13,42 @@ def login_admin_panel():
     driver.find_element_by_name('password').send_keys(PASSWORD)
     driver.find_element_by_name('login').click()
 
+
+def print_log():
+    """Функция вывода на экран ошибок"""
+
+    if len(driver.get_log('browser')) > 0:
+        print('Есть сообщения об ошибках при открытии страницы ' + str(driver.current_url))
+    else:
+        print('На странице ' + str(driver.current_url) + ' сообщений об ошибках нет')
+
+
 def check_log():
+    """функия перебора товаров на странице категории"""
+
     driver.get(CATEGORY_URL)
-    product_rows = driver.find_elements_by_xpath('//tr[@class="row"]//td[3]/a')
-    x = 0
-    for i in product_rows:
-        element = driver.find_elements_by_xpath('//tr[@class="row"]//td[3]/a')[x].click()
-        curent_url = driver.current_url
-        after_click = driver.find_elements_by_xpath('//tr[@class="row"]//td[3]/a')
-        if len(product_rows)< len(after_click):
-            y = x
-            for i in product_rows:
-                driver.find_elements_by_xpath('//tr[@class="row"]//td[3]/a')[y].click()
-                if len(driver.get_log('browser')) > 0:
-                    print('Есть сообщения об ошибках при открытии страницы ' + str(driver.current_url))
-                else:
-                    print('На странице ' + str(driver.current_url) + ' сообщений об ошибках нет')
-                y += 1
-                driver.get(curent_url)
-        driver.get(CATEGORY_URL)
-        x += 1
+    product_rows = driver.find_elements_by_xpath('//table[@class="dataTable"]//tr[@class="row"]//td[3]/a')
+    x=0
+    for row in product_rows:
+        current_url = driver.current_url
+        driver.find_elements_by_xpath('//table[@class="dataTable"]//tr[@class="row"]//td[3]/a')[x].click()
+        print_log()
+        new_table = driver.find_elements_by_xpath('//table[@class="dataTable"]//tr[@class="row"]//td[3]/a')
+        for i in new_table:
+            current_url = driver.current_url
+            new_table = driver.find_elements_by_xpath('//table[@class="dataTable"]//tr[@class="row"]//td[3]/a')
+            driver.find_elements_by_xpath('//table[@class="dataTable"]//tr[@class="row"]//td[3]/a')[x].click()
+            print_log()
+            x+=1
+            driver.get(current_url)
         break
-    driver.quit()
 
 
 def main():
     login_admin_panel()
     check_log()
+    driver.quit()
+
 
 if __name__ == '__main__':
     main()
-
-# def check_log():
-#     driver.get(CATEGORY_URL)
-#     product_rows = driver.find_elements_by_xpath('//tr[@class="row"]//td[3]/a')
-#     x = 0
-#     for i in product_rows:
-#         element = driver.find_elements_by_xpath('//tr[@class="row"]//td[3]/a')[x].click()
-#         sub_elements = driver.find_elements_by_xpath('//tr[@class="row"]//td[3]/a')
-#         if len(sub_elements)>0:
-#             y = 0
-#             for a in sub_elements:
-#                 curent_url = driver.current_url
-#                 driver.find_elements_by_xpath('//tr[@class="row"]//td[3]/a')[y].click()
-#                 y+=1
-#                 time.sleep(2)
-#                 driver.get(curent_url)
-#         time.sleep(2)
-#         driver.get(CATEGORY_URL)
-#         x+=1
